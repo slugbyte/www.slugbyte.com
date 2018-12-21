b0e324263876092e339d4962714791d867ecb3c2
Duncan Marsh <slugbyte@slugbyte.com>
2018-12-21 08:00:40 +0800

    adding day 6 with the space heater blog post
    just the markdown

---
+++ src/data/blog/day-6-with-the-space-heater/metadata.js
@@ -0,0 +1,14 @@
+{
+    "url": "day-6-with-the-space-heater",
+      "title": "/blog/Day 6 with the space heater",
+      "description": "Life update, and a run down on JS object instantion benchmarks.",
+      "keywords": [
+            "Javascript",
+            "NodeJS",
+            "benchmarking",
+            "speed test"
+          ],
+      "imageUrl": "default",
+      "published": true,
+      "timestamp": "2018-12-20T23:57:04.254Z"
+}
