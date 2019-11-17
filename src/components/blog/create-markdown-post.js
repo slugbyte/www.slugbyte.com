7943b6d0f14e3a4110ebdf0adc68cf98c4f4d0bf
Duncan Marsh <slugbyte@slugbyte.com>
2019-11-17 15:24:45 +0800

    update styles

--- src/components/blog/create-markdown-post.js
+++ src/components/blog/create-markdown-post.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import './_markdown-post.scss'
 
+import React from 'react'
 import {Link} from 'gatsby'
 import Layout from '../layout'
 import Markdown from '../markdown'
@@ -7,10 +8,13 @@ import BlogNav from './blog-nav.js'
 
 const createMarkdownPost = ({metadata, content}) => () => {
   return (
+
     <Layout metadata={metadata}>
-      <div className={metadata.url.slice(1).replace(new RegExp('/', 'g'), '-')}>
-        <Markdown content={content} />
-        <BlogNav metadata={metadata} />
+      <div className='blog-post'>
+        <div className={metadata.url.slice(1).replace(new RegExp('/', 'g'), '-')}>
+          <Markdown content={content} />
+          <BlogNav metadata={metadata} />
+        </div>
       </div>
     </Layout>
   )
