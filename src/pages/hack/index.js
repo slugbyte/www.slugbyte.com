755f0187cf8727f04b6f739a27c898c0f7232be3
Duncan Marsh <slugbyte@slugbyte.com>
2019-11-20 14:17:30 +0800

    hack uses lazy-image now

--- src/pages/hack/index.js
+++ src/pages/hack/index.js
@@ -3,11 +3,13 @@ import './_project.scss'
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../../components/layout'
+import LazyImage from '../../components/lazy-image'
 
 let projects = [
   {
     name: 'toy', 
     imageURL: require('../../asset/image/project/500/dusk-500.png'),
+    thumbnail: require('../../asset/image/project/50/dusk-50.png'),
     description: 'A micro virtual machine and assembly language with a WebUI to visulize registers and memory.',
     website: 'https://slugbyte.github.io/toy',
     github: 'https://www.github.com/slugbyte/toy',
@@ -15,6 +17,7 @@ let projects = [
   {
     name: 'mold', 
     imageURL: require('../../asset/image/project/500/lime-500.png'),
+    thumbnail: require('../../asset/image/project/50/lime-50.png'),
     description: 'A powerful system confiuration tool designed with programers in mind.',
     website: null,
     github: 'https://www.github.com/slugbyte/mold',
@@ -22,6 +25,7 @@ let projects = [
   {
     name: 'slugtina', 
     imageURL: require('../../asset/image/project/500/peach-500.png'),
+    thumbnail: require('../../asset/image/project/50/peach-50.png'),
     description: 'A fun paint application.',
     website: 'https://slugbyte.github.io/slugtina',
     github: 'https://www.github.com/slugbyte/slugtina',
@@ -29,6 +33,7 @@ let projects = [
   {
     name: 'config', 
     imageURL: require('../../asset/image/project/500/yellow-500.png'),
+    thumbnail: require('../../asset/image/project/50/yellow-50.png'),
     description: 'My dotfiles and tons of custom scripts and tooling.',
     website: null,
     github: 'https://www.github.com/slugbyte/config',
@@ -36,6 +41,7 @@ let projects = [
   {
     name: 'slugular', 
     imageURL: require('../../asset/image/project/500/sky-500.png'),
+    thumbnail: require('../../asset/image/project/50/sky-50.png'),
     description: 'A proof of concept component architecture for building web applications.',
     website: 'https://slugbyte.github.io/slugular/docs/',
     github: 'https://www.github.com/slugbyte/slugular/docs',
@@ -43,6 +49,7 @@ let projects = [
   {
     name: 'yuejiu', 
     imageURL: require('../../asset/image/project/500/teal-500.png'),
+    thumbnail: require('../../asset/image/project/50/teal-50.png'),
     description: 'a personal vim colorscheme.',
     website: null,
     github: 'https://www.github.com/slugbyte/yuejiu',
@@ -50,6 +57,7 @@ let projects = [
   {
     name: 'bash and gpp', 
     imageURL: require('../../asset/image/project/500/pink-500.png'),
+    thumbnail: require('../../asset/image/project/50/pink-50.png'),
     description: 'A proof of concept for coupling the gnu preprocessor with bash scripts for scripting exppierence taht suports modules.',
     website: null,
     github: 'https://www.github.com/slugbyte/yuejiu',
@@ -57,6 +65,7 @@ let projects = [
   {
     name: 'lawnjunk', 
     imageURL: require('../../asset/image/project/500/creamsicle-500.png'), 
+    thumbnail: require('../../asset/image/project/50/creamsicle-50.png'), 
     description: 'A junkyard of well over 100 repositories of my old projects and teaching demos.',
     website: null,
     github: 'https://www.github.com/lawnjunk',
@@ -70,7 +79,11 @@ const ProjectPostItem = ({project}) => (
     {project.website ? <a href={project.website} >website</a> : ''}
     {project.github? <a href={project.github} >github</a> : '' }
     <span className='clearfix' />
-    <img src={project.imageURL}/>
+    <LazyImage image={{
+      url: project.imageURL,
+      thumbnail: project.thumbnail,
+      description: project.description,
+    }} />
   </div>
 )
 
